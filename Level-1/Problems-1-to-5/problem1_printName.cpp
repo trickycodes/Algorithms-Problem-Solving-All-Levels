508bd7be996e5c79502a744a3a6d589af34d6a98
Tricky Codes <202556386+trickycodes@users.noreply.github.com>
2024-09-30 21:34:41 +0300

    Rename Fix Mistake, 2

--- Level-1/Problems-1-to-5/problem1_printName.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "problem1_printName.h"
-
-void PrintName(std::string name)
-{
-	std::cout << "Hello there, " << name << "!" << std::endl;
-}
--- Level-1/Problems-1-to-5/problem1_printName.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifndef PROBLEM1_PRINTNAME_H
-
-#define PROBLEM1_PRINTNAME_H
-
-void PrintName(std::string name);
-
-#endif
